0a699979fd8592bab41ad90bc57a846d5fcc6f77
Viktor Ferenczi <viktor@ferenczi.eu>
2024-01-03 03:00:53 +0100

    Placeholder and plan for test fixture generation

--- aidev/developer/developer.py
+++ aidev/developer/developer.py
@@ -1,6 +1,5 @@
 import random
 from pprint import pprint
-from typing import List, Optional
 
 from ..developer.junior import Junior
 from ..developer.project import Project
@@ -16,41 +15,61 @@ class Developer:
         self.sonar = sonar
         self.engine = engine
 
-        self.issues: Optional[List[Issue]] = None
-        self.issue: Optional[Issue] = None
-        self.issue_index: int = 0
-
         self.rng = random.Random()
 
-    async def fix_issues(self, branch_name: str):
+    def prepare_working_copy(self, branch_name):
         if self.project.has_changes():
             raise RuntimeError(f'Please make sure there are no changes in your working copy: {self.project.project_dir}')
-
         self.project.ensure_branch(branch_name)
-
         self.project.format_code()
         self.project.stage_change('.')
         if self.project.has_changes():
             self.project.commit('Formatted code')
 
+    async def fix_issues(self, branch_name: str):
+        self.prepare_working_copy(branch_name)
+
+        issues: list[Issue] = []
+
+        def query_open_issues():
+            issues[:] = [i for i in self.sonar.get_issues() if i.status == IssueStatus.OPEN]
+
         self.project.analyze()
-        self.query_open_issues()
+        query_open_issues()
 
         brain = Junior(self.project, self.engine)
-        while self.issues:
-            print(f'Choosing one from the {len(self.issues)} issues')
-            self.issue = self.rng.choice(self.issues)
-            pprint(self.issue)
-            if await brain.fix_issue(self.issue):
+        while issues:
+            print(f'Choosing one from the {len(issues)} issues')
+            issue = self.rng.choice(issues)
+            pprint(issue)
+            if await brain.fix_issue(issue):
                 self.project.stage_change('.')
-                self.project.commit(f'{self.issue.key}: {self.issue.message}')
+                self.project.commit(f'{issue.key}: {issue.message}')
                 self.project.analyze()
-                self.query_open_issues()
+                query_open_issues()
 
         print('No more issues to fix.')
 
-    def query_open_issues(self):
-        self.issues = [issue for issue in self.sonar.get_issues() if issue.status == IssueStatus.OPEN]
-
     async def create_test_fixture(self, branch_name: str):
-        pass
+        await self.prepare_working_copy(branch_name)
+
+        self.project.analyze()
+
+        brain = Junior(self.project, self.engine)
+
+        # Run test coverage analysis
+        # Identify the views not covered yet
+        # For each uncovered view:
+        ## Identify possible HTTP requests to cover with test fixtures, feed controllers+model+view
+        ## Generate those fixtures one by one, give the HomeIndexTests as 1-shot example
+        ## Build and run the test case
+        ## Verify that the output of the fixture is reasonable
+        ## Record the output as reference
+        ## Run the test again, make sure it passes
+        ## Commit the test
+        # Repeat until all views are covered
+
+        while 1:
+            break
+
+        print('All views are covered with test fixtures.')
